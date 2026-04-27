ffd7f0a399efcccff652cadcbce10c6a5fb9fb97
Thomas Sjerps <sjerpsthomas@gmail.com>
2026-04-27 10:47:09 +0200

    Strip down more Yiska content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,34 +1,5 @@
-import { COLORS } from "@lib/color";
-import { SECTION_TYPES } from "@lib/section.ts";
 import { defineCollection, z } from "astro:content";
 import type {SchemaContext} from "astro/content/config";
-
-const sections = defineCollection({
-  type: "content",
-
-  schema: ({ image }: SchemaContext) => z.object({
-    title: z.string(),
-
-    type: z.enum(SECTION_TYPES).default("tekst"),
-
-    title_navbar: z.optional(z.string()),
-
-    achtergrond: z.union([z.enum(COLORS), image()]).default("zwart"),
-    tekstkleur: z.enum(COLORS).default("wit"),
-    tekstkleur_achtergrond: z.enum(COLORS).default("zwart"),
-
-    polaroid_links: z.object({ image: image(), description: z.string() }).optional(),
-    polaroid_rechts: z.object({ image: image(), description: z.string() }).optional(),
-
-    aantal_stickers: z.number().default(4),
-
-    socials: z.optional(z.array(z.object({
-      social: z.string(),
-      name: z.string(),
-      url: z.string(),
-    })))
-  }),
-});
 
 const portfolio = defineCollection({
   type: "content",
@@ -47,4 +18,4 @@ const portfolio = defineCollection({
   }),
 });
 
-export const collections = { portfolio, sections };
+export const collections = { portfolio };

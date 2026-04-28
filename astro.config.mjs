176783737019e9fd2be42db0cbddc43dada914e8
sjerpsthomas <53572785+sjerpsthomas@users.noreply.github.com>
2026-04-29 00:05:12 +0200

    Upgrade Tailwind, add text shadow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 // noinspection ES6PreferShortImport
 import { DEFAULT_LOCALE, LOCALES } from './src/lib/locale.js';
 
 import icon from "astro-icon";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  integrations: [mdx(), sitemap(), icon()],
 
   i18n: {
     defaultLocale: DEFAULT_LOCALE,
@@ -18,5 +19,9 @@ export default defineConfig({
       prefixDefaultLocale: false,
       strategy: 'manual',
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
